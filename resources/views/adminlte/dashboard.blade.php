ec698c6a64fb1cc3cadf694c712797949539da48
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-06-01 17:49:17 +0600

    splitting diferent parts of adminlte dashboard 3

---
+++ resources/views/adminlte/dashboard.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  @include('adminlte.header')
+  <!--
+  `body` tag options:
+
+    Apply one or more of the following classes to to the body tag
+    to get the desired effect
+
+    * sidebar-collapse
+    * sidebar-mini
+  -->
+  <body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+      <!-- Navbar -->
+      @include('adminlte.navbar')
+      <!-- /.navbar -->
+
+      <!-- Main Sidebar Container -->
+      @include('adminlte.sidebar')
+
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        @include('adminlte.content.header')
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        @include('adminlte.content.main-content')
+        <!-- /.content -->
+      </div>
+      <!-- /.content-wrapper -->
+
+      <!-- Control Sidebar -->
+      <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+      </aside>
+      <!-- /.control-sidebar -->
+
+      <!-- Main Footer -->
+      @include('adminlte.footer')
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+    @include('adminlte.script')
+  </body>
+</html>
---
+++ resources/views/adminlte/script.blade.php
@@ -0,0 +1,14 @@
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE -->
+    <script src="admin-lte/dist/js/adminlte.js"></script>
+
+    <!-- OPTIONAL SCRIPTS -->
+    <script src="plugins/chart.js/Chart.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    {{-- Demo js is comment out to prevent test case --}}
+    {{-- <script src="admin-lte/dist/js/demo.js"></script> --}}
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="admin-lte/dist/js/pages/dashboard3.js"></script>
