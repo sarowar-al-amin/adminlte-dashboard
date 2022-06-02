254c466ab3de30b631485c2f0ad34da79882c963
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2022-06-02 14:59:16 +0600

    Making layout page template

--- resources/views/adminlte/dashboard.blade.php
+++ resources/views/adminlte/dashboard.blade.php
@@ -1,46 +1,274 @@
-<!DOCTYPE html>
-<html lang="en">
-  @include('adminlte.header')
-  <!--
-  `body` tag options:
+@extends('adminlte.layout')
+{{-- Moving navbar and sidebar to the main layout file --}}
+{{-- @section('navbar')
+  This is navigation bar
+  @include('adminlte.navbar')
+@endsection
 
-    Apply one or more of the following classes to to the body tag
-    to get the desired effect
+@section('sidebar')
+  this sidebar
+  @include('adminlte.sidebar')
+@endsection --}}
 
-    * sidebar-collapse
-    * sidebar-mini
-  -->
-  <body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-      <!-- Navbar -->
-      @include('adminlte.navbar')
-      <!-- /.navbar -->
+@section('content-header')
+  {{-- This is content header --}}
+  @include('adminlte.content.header')
+@endsection
 
-      <!-- Main Sidebar Container -->
-      @include('adminlte.sidebar')
+@section('main-content')
+  {{-- Main content design --}}
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="card">
+            <div class="card-header border-0">
+              <div class="d-flex justify-content-between">
+                <h3 class="card-title">Online Store Visitors</h3>
+                <a href="javascript:void(0);">View Report</a>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="d-flex">
+                <p class="d-flex flex-column">
+                  <span class="text-bold text-lg">820</span>
+                  <span>Visitors Over Time</span>
+                </p>
+                <p class="ml-auto d-flex flex-column text-right">
+                  <span class="text-success">
+                    <i class="fas fa-arrow-up"></i> 12.5%
+                  </span>
+                  <span class="text-muted">Since last week</span>
+                </p>
+              </div>
+              <!-- /.d-flex -->
 
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        @include('adminlte.content.header')
-        <!-- /.content-header -->
+              <div class="position-relative mb-4">
+                <canvas id="visitors-chart" height="200"></canvas>
+              </div>
 
-        <!-- Main content -->
-        @include('adminlte.content.main-content')
-        <!-- /.content -->
+              <div class="d-flex flex-row justify-content-end">
+                <span class="mr-2">
+                  <i class="fas fa-square text-primary"></i> This Week
+                </span>
+
+                <span>
+                  <i class="fas fa-square text-gray"></i> Last Week
+                </span>
+              </div>
+            </div>
+          </div>
+          <!-- /.card -->
+
+          <div class="card">
+            <div class="card-header border-0">
+              <h3 class="card-title">Products</h3>
+              <div class="card-tools">
+                <a href="#" class="btn btn-tool btn-sm">
+                  <i class="fas fa-download"></i>
+                </a>
+                <a href="#" class="btn btn-tool btn-sm">
+                  <i class="fas fa-bars"></i>
+                </a>
+              </div>
+            </div>
+            <div class="card-body table-responsive p-0">
+              <table class="table table-striped table-valign-middle">
+                <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Sales</th>
+                  <th>More</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>
+                    <img src="admin-lte/dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                    Some Product
+                  </td>
+                  <td>$13 USD</td>
+                  <td>
+                    <small class="text-success mr-1">
+                      <i class="fas fa-arrow-up"></i>
+                      12%
+                    </small>
+                    12,000 Sold
+                  </td>
+                  <td>
+                    <a href="#" class="text-muted">
+                      <i class="fas fa-search"></i>
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src="admin-lte/dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                    Another Product
+                  </td>
+                  <td>$29 USD</td>
+                  <td>
+                    <small class="text-warning mr-1">
+                      <i class="fas fa-arrow-down"></i>
+                      0.5%
+                    </small>
+                    123,234 Sold
+                  </td>
+                  <td>
+                    <a href="#" class="text-muted">
+                      <i class="fas fa-search"></i>
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src="admin-lte/dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                    Amazing Product
+                  </td>
+                  <td>$1,230 USD</td>
+                  <td>
+                    <small class="text-danger mr-1">
+                      <i class="fas fa-arrow-down"></i>
+                      3%
+                    </small>
+                    198 Sold
+                  </td>
+                  <td>
+                    <a href="#" class="text-muted">
+                      <i class="fas fa-search"></i>
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img src="admin-lte/dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                    Perfect Item
+                    <span class="badge bg-danger">NEW</span>
+                  </td>
+                  <td>$199 USD</td>
+                  <td>
+                    <small class="text-success mr-1">
+                      <i class="fas fa-arrow-up"></i>
+                      63%
+                    </small>
+                    87 Sold
+                  </td>
+                  <td>
+                    <a href="#" class="text-muted">
+                      <i class="fas fa-search"></i>
+                    </a>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col-md-6 -->
+        <div class="col-lg-6">
+          <div class="card">
+            <div class="card-header border-0">
+              <div class="d-flex justify-content-between">
+                <h3 class="card-title">Sales</h3>
+                <a href="javascript:void(0);">View Report</a>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="d-flex">
+                <p class="d-flex flex-column">
+                  <span class="text-bold text-lg">$18,230.00</span>
+                  <span>Sales Over Time</span>
+                </p>
+                <p class="ml-auto d-flex flex-column text-right">
+                  <span class="text-success">
+                    <i class="fas fa-arrow-up"></i> 33.1%
+                  </span>
+                  <span class="text-muted">Since last month</span>
+                </p>
+              </div>
+              <!-- /.d-flex -->
+
+              <div class="position-relative mb-4">
+                <canvas id="sales-chart" height="200"></canvas>
+              </div>
+
+              <div class="d-flex flex-row justify-content-end">
+                <span class="mr-2">
+                  <i class="fas fa-square text-primary"></i> This year
+                </span>
+
+                <span>
+                  <i class="fas fa-square text-gray"></i> Last year
+                </span>
+              </div>
+            </div>
+          </div>
+          <!-- /.card -->
+
+          <div class="card">
+            <div class="card-header border-0">
+              <h3 class="card-title">Online Store Overview</h3>
+              <div class="card-tools">
+                <a href="#" class="btn btn-sm btn-tool">
+                  <i class="fas fa-download"></i>
+                </a>
+                <a href="#" class="btn btn-sm btn-tool">
+                  <i class="fas fa-bars"></i>
+                </a>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
+                <p class="text-success text-xl">
+                  <i class="ion ion-ios-refresh-empty"></i>
+                </p>
+                <p class="d-flex flex-column text-right">
+                  <span class="font-weight-bold">
+                    <i class="ion ion-android-arrow-up text-success"></i> 12%
+                  </span>
+                  <span class="text-muted">CONVERSION RATE</span>
+                </p>
+              </div>
+              <!-- /.d-flex -->
+              <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
+                <p class="text-warning text-xl">
+                  <i class="ion ion-ios-cart-outline"></i>
+                </p>
+                <p class="d-flex flex-column text-right">
+                  <span class="font-weight-bold">
+                    <i class="ion ion-android-arrow-up text-warning"></i> 0.8%
+                  </span>
+                  <span class="text-muted">SALES RATE</span>
+                </p>
+              </div>
+              <!-- /.d-flex -->
+              <div class="d-flex justify-content-between align-items-center mb-0">
+                <p class="text-danger text-xl">
+                  <i class="ion ion-ios-people-outline"></i>
+                </p>
+                <p class="d-flex flex-column text-right">
+                  <span class="font-weight-bold">
+                    <i class="ion ion-android-arrow-down text-danger"></i> 1%
+                  </span>
+                  <span class="text-muted">REGISTRATION RATE</span>
+                </p>
+              </div>
+              <!-- /.d-flex -->
+            </div>
+          </div>
+        </div>
+        <!-- /.col-md-6 -->
       </div>
-      <!-- /.content-wrapper -->
-
-      <!-- Control Sidebar -->
-      @include('adminlte.rightsidebar')
-      <!-- /.control-sidebar -->
-
-      <!-- Main Footer -->
-      @include('adminlte.footer')
+      <!-- /.row -->
     </div>
-    <!-- ./wrapper -->
+    <!-- /.container-fluid -->
+  </div>
+@endsection
 
-    <!-- REQUIRED SCRIPTS -->
-    @include('adminlte.script')
-  </body>
-</html>
+{{-- Moving footer part to the main layout file --}}
+{{-- @section('main-footer')
+  Main footer
+  @include('adminlte.footer')
+@endsection --}}
